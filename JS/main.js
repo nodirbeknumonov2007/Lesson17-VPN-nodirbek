f863f4c13f7de1e7bde285836c58fd7c3f611cc4
nodirbeknumonov2007 <nodirbeknumonov2007@gmail.com>
2023-04-03 10:10:10 +0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:25,
+  dots:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true,
+          loop:true
+      },
+      600:{
+          items:2,
+          nav:true,
+          loop:true
+      },
+      1000:{
+          items:2,
+          nav:true,
+          loop:true
+      }
+  }
+})
+
+
+
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.left = "0";
+    // document.getElementById("navbar-responsive").style.top = "0";
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.left = "-100%";
+    // document.getElementById("navbar-responsive").style.top = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
